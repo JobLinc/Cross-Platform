d3ac7bf56e1598875368390127c22a8bd95c72d1
Ahmed Hesham <166301927+ahmedesh22@users.noreply.github.com>
2025-03-06 14:40:30 +0200

    Merge remote-tracking branch 'origin/Post'

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -11,7 +11,7 @@ import 'package:joblinc/features/userprofile/ui/screens/profile_screen.dart';
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     //this arguments to be passed in any screen like this ( arguments as ClassName )
-    final arguments = settings.arguments;
+    // final arguments = settings.arguments;
 
     switch (settings.name) {
       case Routes.onBoardingScreen:
