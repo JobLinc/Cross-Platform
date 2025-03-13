daac5a668b941c3b137d0550ee1904857a4c2627
AbdelrahmanMohamed-Fathy <abdelrahman.fathy04@outlook.com>
2025-03-13 20:41:21 +0200

    created universal bottombar to be used on all main traversable screens

---
+++ lib/core/widgets/universal_bottom_bar.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:joblinc/core/theming/colors.dart';
+import 'package:joblinc/features/home/ui/screens/home_screen.dart';
+
+class UniversalBottomBar extends StatefulWidget {
+  static final UniversalBottomBar _bar = UniversalBottomBar._constructor();
+  const UniversalBottomBar._constructor();
+
+  factory UniversalBottomBar() {
+    return _bar;
+  }
+
+  @override
+  State<UniversalBottomBar> createState() => _UniversalBottomBarState();
+}
+
+class _UniversalBottomBarState extends State<UniversalBottomBar> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Divider(
+          thickness: 1,
+          height: 0,
+        ),
+        BottomNavigationBar(
+          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+          currentIndex: _selectedIndex,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: ColorsManager.darkBurgundy,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.people), label: "My Network"),
+            BottomNavigationBarItem(icon: Icon(Icons.add_box), label: "Post"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), label: "Notifications"),
+            BottomNavigationBarItem(icon: Icon(Icons.work), label: "Jobs"),
+          ],
+          onTap: (value) {
+            setState(() {
+              _selectedIndex = value;
+            });
+            switch (value) {
+              case 0:
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()));
+              case 1:
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Placeholder()));
+              case 2:
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Placeholder()));
+              case 3:
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Placeholder()));
+              case 4:
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Placeholder()));
+              default:
+                throw UnimplementedError();
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:joblinc/core/routing/app_router.dart';
 import 'package:joblinc/core/routing/routes.dart';
-import 'package:joblinc/core/theming/colors.dart';
 import 'package:joblinc/features/home/data/models/post_model.dart';
+import 'package:joblinc/core/widgets/universal_bottom_bar.dart';
 import 'package:joblinc/features/home/ui/widgets/post_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -66,72 +65,7 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: BottomBar(),
-    );
-  }
-}
-
-class BottomBar extends StatefulWidget {
-  const BottomBar({
-    super.key,
-  });
-
-  @override
-  State<BottomBar> createState() => _BottomBarState();
-}
-
-class _BottomBarState extends State<BottomBar> {
-  int _selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Divider(
-          thickness: 1,
-          height: 0,
-        ),
-        BottomNavigationBar(
-          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-          currentIndex: _selectedIndex,
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: ColorsManager.darkBurgundy,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.people), label: "My Network"),
-            BottomNavigationBarItem(icon: Icon(Icons.add_box), label: "Post"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.notifications), label: "Notifications"),
-            BottomNavigationBarItem(icon: Icon(Icons.work), label: "Jobs"),
-          ],
-          onTap: (value) {
-            setState(() {
-              _selectedIndex = value;
-            });
-            switch (value) {
-              case 0:
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()));
-              case 1:
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Placeholder()));
-              case 2:
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Placeholder()));
-              case 3:
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Placeholder()));
-              case 4:
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Placeholder()));
-              default:
-                throw UnimplementedError();
-            }
-          },
-        ),
-      ],
+      bottomNavigationBar: UniversalBottomBar(),
     );
   }
 }
