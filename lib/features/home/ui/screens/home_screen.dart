1f75cc28fd987b59a744746741fa517ec15a6665
abdelrahmansameh092@gmail.com <abdelrahmansameh092@gmail.com>
2025-03-17 13:32:31 +0200

    feat: universal app bar integrated

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:joblinc/core/routing/routes.dart';
+import 'package:joblinc/core/widgets/custom_search_bar.dart';
 import 'package:joblinc/features/home/data/models/post_model.dart';
 import 'package:joblinc/core/widgets/universal_bottom_bar.dart';
 import 'package:joblinc/features/home/ui/widgets/post_widget.dart';
@@ -14,59 +15,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-  void _onItemTap(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
+  TextEditingController searchController= TextEditingController();
   @override
   Widget build(BuildContext context) {
 
 
-    List<HomeScreenInput> mainScreens=[
-      HomeScreenInput(
-        searchKeyName: "",
-        searchText: "search",
-        searchOnPress: emptyFunction,
-        searchOnTextChange: emptyFunction,
-        searchTextController: SearchController(),
-        body:HomeScreenBody()
-      ),
-        HomeScreenInput(
-        searchKeyName: "",
-        searchText: "search",
-        searchOnPress: emptyFunction,
-        searchOnTextChange: emptyFunction,
-        searchTextController: SearchController(),
-        body:HomeScreenBody()
-      ),
-        HomeScreenInput(
-        searchKeyName: "",
-        searchText: "search",
-        searchOnPress: emptyFunction,
-        searchOnTextChange: emptyFunction,
-        searchTextController: SearchController(),
-        body:HomeScreenBody()
-      ),
-        HomeScreenInput(
-        searchKeyName: "",
-        searchText: "search",
-        searchOnPress: emptyFunction,
-        searchOnTextChange: emptyFunction,
-        searchTextController: SearchController(),
-        body:HomeScreenBody()
-      ),
-        HomeScreenInput(
-        searchKeyName: "jobList_search_textField",
-        searchText: "search jobs",
-        searchOnPress:()=>goToJobSearch(context),
-        searchOnTextChange: emptyFunction,
-        searchTextController: SearchController(),
-        body:JobListScreen()
-      ),
-    ];
+
 
     return Scaffold(
       appBar: AppBar(
@@ -83,26 +38,28 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Center(
               child: Semantics(
                 label: 'home_topBar_search',
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search',
-                    prefixIcon: Icon(Icons.search, color: Colors.grey),
-                    border: InputBorder.none,
+                child:CustomSearchBar(
+                      keyName:'home_topBar_search', 
+                      text: 'search',
+                      onPress: (){},
+                      onTextChange:  (){},
+                      controller: searchController),
                   ),
                 ),
               ),
             ),
+          actions: [
+          IconButton(
+            icon: Icon(Icons.message, color: Colors.black),
+            onPressed: () {
+              Navigator.pushNamed(context, Routes.chatListScreen);   
+            },
           ),
-        ),
-        actions: [
-          Semantics(
-            label: 'home_topBar_chatButton',
-            child: IconButton(
-              icon: Icon(Icons.message, color: Colors.black),
-              onPressed: () {
-                Navigator.pushNamed(context, Routes.chatListScreen);
-              },
-            ),
+          IconButton(
+            icon: Icon(FontAwesomeIcons.crown, color: Colors.black),
+            onPressed: () {
+              Navigator.pushNamed(context, Routes.premiumScreen);
+            },
           ),
         ],
       ),
@@ -121,27 +78,3 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 
-class HomeScreenInput{
-  late String searchKeyName;
-  late String searchText;
-  late VoidCallback searchOnPress;
-  late Function searchOnTextChange;
-  late TextEditingController searchTextController;
-  late Widget body;
-
-    HomeScreenInput({
-      required this.searchKeyName,
-      required this.searchText,
-      required this.searchOnPress,
-      required this.searchOnTextChange,
-      required this.searchTextController,
-      required this.body
-    });
-}
-
-
-
-void emptyFunction(){}
-void goToJobSearch(BuildContext context){
-  Navigator.pushNamed(context,Routes.jobSearchScreen);
-  }
