fff4dab59cd5f5c2cedb642b9c38871ebc217e02
Ahmed Hesham <166301927+ahmedesh22@users.noreply.github.com>
2025-03-21 23:49:09 +0200

    Modify: modify forget password UI

--- lib/features/forgetpassword/ui/screens/forgetpassword_screen.dart
+++ lib/features/forgetpassword/ui/screens/forgetpassword_screen.dart
@@ -1,11 +1,179 @@
 import 'package:flutter/material.dart';
-import 'package:joblinc/features/forgetpassword/ui/screens/enter_email_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:joblinc/core/theming/colors.dart';
+import 'package:joblinc/features/forgetpassword/logic/cubit/forget_password_cubit.dart';
+import 'package:joblinc/features/forgetpassword/logic/cubit/forget_password_state.dart';
 
-class ForgetpasswordScreen extends StatelessWidget {
-  const ForgetpasswordScreen({super.key});
+class ForgotPasswordSteps extends StatefulWidget {
+  const ForgotPasswordSteps({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordStepsState createState() => _ForgotPasswordStepsState();
+}
+
+class _ForgotPasswordStepsState extends State<ForgotPasswordSteps> {
+  int _currentStep = 0;
+  String email = '';
+  String forgotToken = '';
+  String resetToken = '';
+
+  final emailController = TextEditingController();
+  final otpController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return EnterEmailView();
+    return BlocConsumer<ForgetPasswordCubit, ForgotPasswordState>(
+      listener: (context, state) {
+        if (state is ForgotPasswordEmailSent) {
+          forgotToken = state.forgotToken;
+          setState(() => _currentStep = 1);
+        } else if (state is ForgotPasswordOtpVerified) {
+          resetToken = state.resetToken;
+          setState(() => _currentStep = 2);
+        } else if (state is ForgotPasswordSuccess) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Password reset successfully!")),
+          );
+          Navigator.pop(context);
+        } else if (state is ForgotPasswordError) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(state.message)),
+          );
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Forgot Password"),
+            backgroundColor: ColorsManager.softMutedSilver,
+            elevation: 10,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Stepper(
+              connectorColor: WidgetStateColor.resolveWith(
+                  (states) => ColorsManager.crimsonRed),
+              type: StepperType.vertical,
+              currentStep: _currentStep,
+              onStepContinue: _handleContinue,
+              onStepCancel: _handleCancel,
+              controlsBuilder: (BuildContext context, ControlsDetails details) {
+                return Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: details.onStepContinue,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: ColorsManager.crimsonRed,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: const Text('Continue'),
+                    ),
+                    const SizedBox(width: 8),
+                    TextButton(
+                      onPressed: details.onStepCancel,
+                      style: TextButton.styleFrom(
+                        foregroundColor: ColorsManager.crimsonRed,
+                      ),
+                      child: const Text('Cancel'),
+                    ),
+                  ],
+                );
+              },
+              stepIconBuilder: (index, state) {
+                return CircleAvatar(
+                  backgroundColor: _currentStep == index
+                      ? ColorsManager.crimsonRed
+                      : Colors.grey,
+                  child: Text('${index + 1}'),
+                );
+              },
+              steps: [
+                Step(
+                  title: const Text("Enter Email"),
+                  content: TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ColorsManager.darkBurgundy)),
+                        labelStyle: TextStyle(color: ColorsManager.mutedSilver),
+                        labelText: "Email",
+                        focusColor: ColorsManager.crimsonRed,
+                        fillColor: ColorsManager.crimsonRed,
+                        hoverColor: ColorsManager.crimsonRed),
+                  ),
+                  isActive: _currentStep >= 0,
+                  state:
+                      _currentStep > 0 ? StepState.complete : StepState.indexed,
+                ),
+                Step(
+                  title: const Text("Enter OTP"),
+                  content: TextField(
+                    style: TextStyle(letterSpacing: 20.sp, fontSize: 30.sp),
+                    controller: otpController,
+                    decoration: const InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ColorsManager.darkBurgundy)),
+                        labelStyle: TextStyle(color: ColorsManager.mutedSilver),
+                        labelText: "OTP",
+                        focusColor: ColorsManager.crimsonRed,
+                        fillColor: ColorsManager.crimsonRed,
+                        hoverColor: ColorsManager.crimsonRed),
+                    maxLength: 6,
+                  ),
+                  isActive: _currentStep >= 1,
+                  state:
+                      _currentStep > 1 ? StepState.complete : StepState.indexed,
+                ),
+                Step(
+                  title: const Text("Reset Password"),
+                  content: TextField(
+                    controller: passwordController,
+                    decoration:
+                        const InputDecoration(labelText: "New Password"),
+                    obscureText: true,
+                  ),
+                  isActive: _currentStep >= 2,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _handleContinue() {
+    if (_currentStep == 0) {
+      email = emailController.text.trim();
+      context.read<ForgetPasswordCubit>().sendEmail(email);
+    } else if (_currentStep == 1) {
+      final otp = otpController.text.trim();
+      context.read<ForgetPasswordCubit>().verifyOtp(email, forgotToken, otp);
+    } else if (_currentStep == 2) {
+      final newPassword = passwordController.text.trim();
+      context
+          .read<ForgetPasswordCubit>()
+          .resetPassword(email, newPassword, resetToken);
+    }
+  }
+
+  void _handleCancel() {
+    if (_currentStep == 0) {
+      Navigator.pop(context); // Exit the screen
+    } else {
+      setState(() => _currentStep -= 1); // Go back one step
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    otpController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 }
--- lib/features/login/data/repos/login_repo.dart
+++ lib/features/login/data/repos/login_repo.dart
@@ -1,5 +1,6 @@
+import 'package:joblinc/core/di/dependency_injection.dart';
 import 'package:joblinc/features/login/data/services/login_api_service.dart';
-import 'package:joblinc/features/login/data/services/securestorage_service.dart';
+import 'package:joblinc/core/helpers/auth_service.dart';
 
 class LoginRepo {
   final LoginApiService _loginApiService;
@@ -8,10 +9,11 @@ class LoginRepo {
 
   Future<void> login(String email, String password) async {
     final response = await _loginApiService.login(email, password);
- 
-    await SecureStorage.saveTokens(
-      accessToken: response.accessToken,
-      refreshToken: response.refreshToken,
-    );
+    final AuthService authService = getIt<AuthService>();
+    await authService.saveAuthInfo(
+        accessToken: response.accessToken,
+        refreshToken: response.refreshToken,
+        userId: response.userId,
+        role: response.role);
   }
 }
