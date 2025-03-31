a547dd8ea3cb3ef9fa110eeb047bd106bce79511
abdelrahmansameh092@gmail.com <abdelrahmansameh092@gmail.com>
2025-03-31 04:19:23 +0200

    feat:job  search , application and details screens and resumes request handelling

--- lib/features/jobs/data/repos/job_repo.dart
+++ lib/features/jobs/data/repos/job_repo.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:joblinc/features/jobs/data/models/job_application_model.dart';
 import 'package:joblinc/features/jobs/data/models/job_model.dart';
 import 'package:joblinc/features/jobs/data/services/job_api_service.dart';
+import 'package:joblinc/features/jobs/ui/screens/job_search_screen.dart';
 
 class JobRepo {
 
@@ -34,6 +37,21 @@ class JobRepo {
     return jobs;
   }
 
+  Future<List<Job>> getSearchedJobs(String keyword,String? location,Filter? filter) async{
+    final response = await _jobApiService.getSearchedJobs(keyword,location,filter);
+    final List<Job> jobs = (response.data as List)
+        .map((jobJson) => Job.fromJson(jobJson as Map<String, dynamic>))
+        .toList();
+    return jobs;
+  }
+
+  Future<List<Resume>> getAllResumes() async {
+    final response = await _jobApiService.getAllResumes();
+    final List<Resume> resumes = (response.data as List)
+        .map((resumeJson) => Resume.fromJson(resumeJson as Map<String, dynamic>))
+        .toList();
+    return resumes;
+  }
 
   Future<Job> createJob(Job job) async {
     final response = await _jobApiService.createJob(job);
@@ -41,13 +59,26 @@ class JobRepo {
   }
 
 
-  Future<Job> saveJob(String jobId) async {
-    final response = await _jobApiService.saveJob(jobId);
-    return Job.fromJson(response as Map<String, dynamic>);
+  Future<void> saveJob(int jobId) async {
+    await _jobApiService.saveJob(jobId);
+    //return Job.fromJson(response as Map<String, dynamic>);
+  }
+  Future<void> unsaveJob(int jobId) async {
+    await _jobApiService.unsaveJob(jobId);
+    //return Job.fromJson(response as Map<String, dynamic>);
   }
 
-  Future<Job> applyJob(String jobId, JobApplication jobApplication) async {
-    final response = await _jobApiService.applyJob(jobId,jobApplication);
-    return Job.fromJson(response as Map<String, dynamic>);
+  Future<void> applyJob(int jobId, JobApplication jobApplication) async {
+    //final response =
+    await _jobApiService.applyJob(jobId,jobApplication);
+    //return Job.fromJson(response as Map<String, dynamic>);
   }
+
+
+  Future<void> uploadResume(File resumeFile) async{
+    await _jobApiService.uploadResume(resumeFile);
+  }
+
+
+
 }
