6379952a82abe9773d862e522fe3da1171aa4979
abdelrahmansameh092@gmail.com <abdelrahmansameh092@gmail.com>
2025-03-31 04:20:42 +0200

    feat: my jobs screen cubits and state management

---
+++ lib/features/jobs/logic/cubit/my_jobs_cubit.dart
@@ -0,0 +1,45 @@
+import 'package:bloc/bloc.dart';
+import 'package:joblinc/features/jobs/data/models/job_application_model.dart';
+import 'package:joblinc/features/jobs/data/models/job_model.dart';
+import 'package:joblinc/features/jobs/data/repos/job_repo.dart';
+import 'package:joblinc/features/jobs/data/services/job_api_service.dart';
+import 'package:meta/meta.dart';
+
+part 'my_jobs_state.dart';
+
+class MyJobsCubit extends Cubit<MyJobsState> {
+  final JobRepo jobRepo;
+
+  List<Job> _jobs =[];
+
+  MyJobsCubit(this.jobRepo) : super(MyJobsInitial());
+
+  Future<void> getSavedJobs() async{
+    emit(MyJobsLoading());
+    try {
+      _jobs=await jobRepo.getSavedJobs();
+      if (_jobs.isEmpty){
+        emit(MySavedJobsEmpty());
+      } else{
+        emit(MySavedJobsLoaded(savedJobs: _jobs ));
+      }
+    } catch (e) {
+      emit(MyJobsErrorLoading(e.toString()));
+    } 
+  }
+
+
+    Future<void> getAppliedJobs() async{
+    emit(MyJobsLoading());
+    try {
+      _jobs=await jobRepo.getAppliedJobs();
+      if (_jobs.isEmpty){
+        emit(MyAppliedJobsEmpty());
+      } else{
+        emit(MyAppliedJobsLoaded(appliedJobs: _jobs ));
+      }
+    } catch (e) {
+      emit(MyJobsErrorLoading(e.toString()));
+    } 
+  }
+}
