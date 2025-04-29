f2c943de1e9adf0f05f352e38973a05445c5938e
AbdelrahmanMohamed-Fathy <abdelrahman.fathy04@outlook.com>
2025-04-29 20:09:57 +0300

    updated post list to expose post settings

--- lib/features/posts/ui/widgets/post_list.dart
+++ lib/features/posts/ui/widgets/post_list.dart
@@ -3,14 +3,25 @@ import 'package:joblinc/features/posts/data/models/post_model.dart';
 import 'package:joblinc/features/posts/ui/widgets/post_widget.dart';
 
 class PostList extends StatelessWidget {
-  const PostList({super.key, required this.posts});
+  const PostList({
+    super.key,
+    required this.posts,
+    this.showExtraMenu = true,
+    this.showOwnerMenu = false,
+  });
   final List<PostModel> posts;
+  final bool showExtraMenu;
+  final bool showOwnerMenu;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: posts.length,
-      itemBuilder: (context, index) => Post(data: posts[index]),
+      itemBuilder: (context, index) => Post(
+        data: posts[index],
+        showExtraMenu: showExtraMenu,
+        showOwnerMenu: showOwnerMenu,
+      ),
     );
   }
 }
