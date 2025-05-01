8a7ce9d155a75d93672057527da2c50caa636e55
AbdelrahmanMohamed-Fathy <abdelrahman.fathy04@outlook.com>
2025-05-01 16:27:52 +0300

    added scale to admin map

--- lib/features/adminpanel/ui/widgets/map_chart.dart
+++ lib/features/adminpanel/ui/widgets/map_chart.dart
@@ -1,6 +1,7 @@
 import 'package:countries_world_map/countries_world_map.dart';
 import 'package:countries_world_map/data/maps/world_map.dart';
 import 'package:flutter/material.dart';
+import 'package:joblinc/core/widgets/custom_snackbar.dart';
 
 class MapChart extends StatelessWidget {
   const MapChart({
@@ -19,15 +20,54 @@ class MapChart extends StatelessWidget {
     viewTransformationController.value.setEntry(0, 3, -xTranslate);
     viewTransformationController.value.setEntry(1, 3, -yTranslate);
 
-    return InteractiveViewer(
-      constrained: false,
-      transformationController: viewTransformationController,
-      child: SimpleMap(
-        countryBorder: CountryBorder(color: Colors.black),
-        instructions: SMapWorld.instructions,
-        defaultColor: Colors.grey,
-        // colors: SMapWorldColors(eG: Colors.purple).toMap(),
-      ),
+    return Stack(
+      children: [
+        InteractiveViewer(
+          constrained: false,
+          transformationController: viewTransformationController,
+          child: SimpleMap(
+            countryBorder: CountryBorder(color: Colors.white),
+            instructions: SMapWorld.instructions,
+            defaultColor: Colors.grey,
+            callback: (id, name, tapDetails) {
+              CustomSnackBar.show(
+                context: context,
+                message: id,
+                type: SnackBarType.info,
+              );
+            },
+            // colors: SMapWorldColors(eG: Colors.purple).toMap(),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(5.0),
+          alignment: Alignment.bottomRight,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            spacing: 5,
+            children: [
+              Text('Scale'),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.yellow,
+                      Colors.purple,
+                    ],
+                  ),
+                ),
+                child: SizedBox(
+                  width: 20,
+                  height: 50,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
